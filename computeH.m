a23d0659af9dc5b7b88b4663cf4658b633cdd9c6
M2neko <56237903+M2neko@users.noreply.github.com>
2020-05-14 02:34:04 -0700

    Update computeH.m

--- computeH.m
+++ computeH.m
@@ -1,24 +1,20 @@
 % part (b)
 function H = computeH(t1, t2)
-%Find out arrangement
-%t1 = t1';
-%t2 = t2';
-n = size(t1, 1);
+n = size(t1, 2);
 if n < 4
     msg = 'Too few points. Need at least 4.';
     error(msg)
 end
 A = zeros(2 * n, 9);
 for i = 1:n
-    x = t1(i,1);
-    y = t1(i,2);
-    xx = t2(i,1);
-    yy = t2(i,2);
-    A(2 * n - 1,:) = [x y 1 0 0 0 -xx*x -xx*y -xx];
-    A(2 * n,:) = [0 0 0 x y 1 -yy*x -yy*y -yy];
+    x = t1(1,i);
+    y = t1(2,i);
+    xx = t2(1,i);
+    yy = t2(2,i);
+    A(2 * i - 1,:) = [x y 1 0 0 0 -xx*x -xx*y -xx];
+    A(2 * i,:) = [0 0 0 x y 1 -yy*x -yy*y -yy];
 end
-
-h = eigs(A*A', 1, 'smallestabs');
-H = reshape(h,3,3);
-%H = H';
+[V,D] = eig(A'*A);
+[~,k] = min(diag(D));
+H = reshape(V(:,k),[3,3])';
 end
