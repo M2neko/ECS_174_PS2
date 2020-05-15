844295b2ef0af41d70ab80dda5edde3fd5ec2920
M2neko <56237903+M2neko@users.noreply.github.com>
2020-05-14 18:09:31 -0700

    Finish warpImage.m

--- warpImage.m
+++ warpImage.m
@@ -31,22 +31,23 @@ warpIm_maxx = round(max([corners(1,1),corners(2,1),corners(3,1),corners(4,1),siz
 warpIm_maxy = round(max([corners(1,2),corners(2,2),corners(3,2),corners(4,2),size(double(refIm),2)]));
 [x,y] = meshgrid(warpIm_minx:warpIm_maxx, warpIm_miny:warpIm_maxy);
 
-
-[width,height] = size(x);
+[h,w] = size(x);
 p = [x(:),y(:)];
 
 origin = [p';ones(1,size(p',2))];
 
-p = H \ origin;
-xpr = p(1,:)./p(3,:);
-ypr = p(2,:)./p(3,:);
+re = H \ origin;
+xpr = re(1,:) ./ re(3,:);
+ypr = re(2,:) ./ re(3,:);
 
-x = reshape(xpr,width,height);
-y = reshape(ypr,width,height);
-R = interp2(double(inputIm(:,:,1)),x,y);
-G = interp2(double(inputIm(:,:,2)),x,y);
-B = interp2(double(inputIm(:,:,3)),x,y);
+box1 = reshape(xpr,h,w);
+box2 = reshape(ypr,h,w);
+R = interp2(double(inputIm(:,:,1)),box1,box2);
+G = interp2(double(inputIm(:,:,2)),box1,box2);
+B = interp2(double(inputIm(:,:,3)),box1,box2);
 warpIm = cat(3,R,G,B);
+
+warpIm(isnan(warpIm)) = 0;
 
 figure;
 imshow(uint8(warpIm));
@@ -67,31 +68,56 @@ new_wap = zeros(nheight,nwidth,3);
 mergeIm = zeros(nheight,nwidth,3);
 
 
-ref_x = max([1,1-warpIm_minx]);
-ref_y = max([1,1-warpIm_miny]);
+ref_x = max([1,1 - warpIm_minx]);
+ref_y = max([1,1 - warpIm_miny]);
 wap_x = max([1,warpIm_minx]);
 wap_y = max([1,warpIm_miny]);
 
 new_ref(ref_y:ref_y + rr - 1,ref_x:ref_x + rc - 1,:) = refIm(:,:,:);
 new_wap(wap_y:wap_y + wr - 1,wap_x:wap_x + wc - 1,:) = warpIm(:,:,:);
 
+
 for i = 1:nheight
     for j = 1:nwidth
-        if (new_ref(i,j,1) > 0 || new_ref(i,j,2) > 0 || new_ref(i,j,3) > 0)
+        if new_ref(i,j,1) > 0 || new_ref(i,j,2) > 0 || new_ref(i,j,3) > 0
             mergeIm(i,j,:) = new_ref(i,j,:);
         end
-        if (new_wap(i,j,1) > 0 || new_wap(i,j,2) > 0 || new_wap(i,j,3) > 0)
+        if new_wap(i,j,1) > 0 || new_wap(i,j,2) > 0 || new_wap(i,j,3) > 0
             mergeIm(i,j,:) = new_wap(i,j,:);
         end
     end
 end
+
+% Also work but slow?
+%{
+for i = 1:nheight
+    for j = 1:nwidth
+        if ~isequal(new_ref(i,j,:),double(zeros(1,1,3))) 
+            mergeIm(i,j,:) = new_ref(i,j,:);
+        end
+        if ~isequal(new_wap(i,j,:),double(zeros(1,1,3))) 
+            mergeIm(i,j,:) = new_wap(i,j,:);
+        end
+    end
+end
+%}
+
+% Test 50% blend
+%{
+for i = 1:nheight
+    for j = 1:nwidth
+        mergeIm(i,j,:) = new_ref(i,j,:) / 2 + new_wap(i,j,:) / 2;
+    end
+end
+%}
+
 
 % Also work but slow
 %{
 for i = 1:nheight
     for j = 1:nwidth
         mergeIm(i,j,:) = new_ref(i,j,:);
-        if (new_wap(i,j,1) > 0 || new_wap(i,j,2) > 0 || new_wap(i,j,3) > 0)
+        if new_wap(i,j,1) > 0 || new_wap(i,j,2) > 0 || new_wap(i,j,3) > 0
             mergeIm(i,j,:) = new_wap(i,j,:);
         end
     end
